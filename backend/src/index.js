3ea9b115a0fde973d6c32f4c7d6c289a7c7e66d1
clsyan <yanvictor442@gmail.com>
2020-03-28 12:55:11 -0300

    primeiro commit

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(cors());
+
+
+app.listen(3333);
+
+
+
+
+/**
+ * localhost/users = rota/recurso
+ * Métodos HTTP:
+ * 
+ *  GET: Buscar dado do back-end
+ *  POST: Enviar dado ao back-end
+ *  PUT: Alterar dado no back-end
+ *  DELETE: deletar dado no back-end
+ *
+ * 
+ * Tipos de parâmetros:
+ *  Query: Parâmetros nomeados enviados na rota após o símbolo de interrogação e geralmente eles servem para filtros, paginação;
+ *  Route Params: Parâmetros usados para identificar e buscar em recursos. Ex.: app.get('/users/:id') o parâmetro passado será o id dos usuários ('/users/3/');
+ *  Request Body: O formato do request. Ex.: Json;
+ * 
+ * Banco de dados:
+ *  SQLite; 
+ *  Driver: SELECT * FROM Users;
+ *  Query Builder: usa javascript;
+ */
