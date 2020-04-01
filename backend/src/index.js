c20dfbbe6ab6e3de7a384dd467f655072635a856
clsyan <yanvictor442@gmail.com>
2020-04-01 15:45:21 -0300

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,12 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+
+app.use(cors({
+    
+    origin: 'http://localhost:3333',
+    credentials: true
+  }));
 
 
 app.listen(3333);
